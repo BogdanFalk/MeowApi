aab1965d382cd541dbf26ce0a30f000c6d0c835c
Bogdan Falk <bogdan@digitalgarden.ro>
2023-09-25 22:53:15 +0300

    Serving Frontend

--- app.js
+++ app.js
@@ -52,6 +52,15 @@ app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 app.use('/reviews', reviewRoutes);
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
