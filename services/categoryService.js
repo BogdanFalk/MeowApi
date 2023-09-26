8520e3d1b4bf46291cd424ac84f1402966c3ec7a
Bogdan Falk <bogdan@digitalgarden.ro>
2023-09-26 13:06:42 +0300

    categories pls

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,23 +1,34 @@
-const Category = require('../models/Category');
+const Category = require("../models/Category");
 
 const getAllCategories = async () => {
   const categories = await Category.find().lean();
-  
+
+  console.log("Categories from DB:", categories); // Check if categories are retrieved from the DB
+
   const mapToHierarchy = (categories, parentId = null) => {
     const result = [];
-    categories.filter(category => category.parentId === parentId).forEach(category => {
-      const children = mapToHierarchy(categories, category._id.toString());
-      if (children.length) {
-        category.children = children;
-      }
-      result.push(category);
-    });
+
+    categories
+      .filter((category) => String(category.parentId) === String(parentId))
+      .forEach((category) => {
+        console.log("Mapping category:", category); // Check which categories are being processed
+
+        const children = mapToHierarchy(categories, category._id.toString());
+        if (children.length) {
+          category.children = children;
+        }
+        result.push(category);
+      });
+
     return result;
   };
-  
-  return mapToHierarchy(categories);
+
+  const hierarchicalCategories = mapToHierarchy(categories);
+  console.log("Hierarchical Categories:", hierarchicalCategories); // Check the final hierarchical structure
+
+  return hierarchicalCategories;
 };
 
 module.exports = {
-  getAllCategories
+  getAllCategories,
 };
