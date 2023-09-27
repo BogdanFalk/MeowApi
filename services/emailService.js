e688ae025c377e4e8cb8e6ee8c9bf0ffe714a98f
Bogdan Falk <bogdan@digitalgarden.ro>
2023-09-27 17:29:34 +0300

    fkamail

--- services/emailService.js
+++ services/emailService.js
@@ -1,25 +1,26 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "apikey",
-    pass: "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",
-  },
-});
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (from, to, subject, text, html) => {
+  const msg = {
+    from: from ? from : "bogdan37falk@gmail.com",
+    to,
+    subject,
+    text,
+    html,
+  };
+
   try {
-    await transporter.sendMail({
-      from: from ? from : "bogdan37falk@gmail.com",
-      to,
-      subject,
-      text,
-      html,
-    });
-    return true;
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+        return true;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  
   } catch (error) {
     console.error("Error sending email:", error);
     return false;
