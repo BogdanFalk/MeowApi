161a144fcf61d648cab57aede3a0789d237155a7
Bogdan Falk <bogdan@digitalgarden.ro>
2023-09-28 13:26:46 +0300

    message

--- services/emailService.js
+++ services/emailService.js
@@ -1,5 +1,7 @@
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey("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");
+sgMail.setApiKey(
+  "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"
+);
 
 const sendEmail = async (from, to, subject, text, html) => {
   const msg = {
@@ -10,21 +12,8 @@ const sendEmail = async (from, to, subject, text, html) => {
     html,
   };
 
-  try {
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log("Email sent");
-        return true;
-      })
-      .catch((error) => {
-        return false
-      });
-  
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return false;
-  }
+  result = await sgMail.send(msg);
+  console.log(result);
 };
 
 module.exports = {
